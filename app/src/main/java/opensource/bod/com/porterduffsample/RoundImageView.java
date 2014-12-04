6c95c26ec22813a18b6c41c386019120c110f91d
Bo <titanduan1083@gmail.com>
2014-12-03 21:07:39 -0800

    Add png files.

---
+++ app/src/main/java/opensource/bod/com/porterduffsample/RoundImageView.java
@@ -0,0 +1,38 @@
+package opensource.bod.com.porterduffsample;
+
+import android.annotation.TargetApi;
+import android.content.Context;
+import android.graphics.Canvas;
+import android.os.Build;
+import android.util.AttributeSet;
+import android.view.View;
+
+/**
+ * Created by bod on 12/3/2014.
+ */
+public class RoundImageView extends View{
+
+    public RoundImageView(Context context) {
+        super(context);
+    }
+
+    public RoundImageView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public RoundImageView(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+
+    @TargetApi(Build.VERSION_CODES.LOLLIPOP)
+    public RoundImageView(Context context, AttributeSet attrs, int defStyleAttr, int defStyleRes) {
+        super(context, attrs, defStyleAttr, defStyleRes);
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+
+
+        super.onDraw(canvas);
+    }
+}
